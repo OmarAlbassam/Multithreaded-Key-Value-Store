ae42fcdd7bbacdcd39b1b0997649c11b1fddad17
Omar Albassam <omar59056@gmail.com>
2026-02-18 22:13:08 +0300

    feat(Datastore): make hashmap sharder to support multithreading

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,51 +1,89 @@
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DataStore<T> {
-    
-    private ConcurrentHashMap<String, String> hashMap;
+
+    private final int NUM_SHARDS = 10; // decide to be number of cores - m1 pro -> 10 cores
+    private final HashMap<String, String>[] shards;
+    private final ReadWriteLock[] locks;
 
     public DataStore() {
-        this.hashMap = new ConcurrentHashMap<String, String>();
+        shards = new HashMap[NUM_SHARDS];
+        locks = new ReadWriteLock[NUM_SHARDS];
+        for (int i = 0; i < NUM_SHARDS; i++) {
+            shards[i] = new HashMap<>();
+            locks[i] = new ReentrantReadWriteLock();
+        }
     }
 
     public String handler(String message) {
         Massege mappedMassege = mapMessage(message);
 
         switch (mappedMassege.getOperation().toUpperCase()) {
-            case "GET": return handleGet(mappedMassege);
-            case "SET": return handleSet(mappedMassege);
-            case "DEL": return handleDel(mappedMassege);
+            case "GET":
+                return handleGet(mappedMassege);
+            case "SET":
+                return handleSet(mappedMassege);
+            case "DEL":
+                return handleDel(mappedMassege);
             default:
                 return "Error";
         }
     }
-    
+
     private Massege mapMessage(String message) {
         String temp[] = message.split(" ");
-        if(temp.length == 2)
+        if (temp.length == 2) {
             return new Massege(temp[0], temp[1], null);
+        }
         return new Massege(temp[0], temp[1], temp[2]);
     }
 
     private String handleSet(Massege mappedMassege) {
-        hashMap.put(mappedMassege.getKey(), mappedMassege.getValue());
+        // to get which hashmap - hashcode the key -> module with number of shards -> hashmap array index
+        int index = Math.abs(mappedMassege.getKey().hashCode()) % NUM_SHARDS;
+        try {
+            locks[index].writeLock().lock();
+            shards[index].put(mappedMassege.getKey(), mappedMassege.getValue());
+        } finally {
+            locks[index].writeLock().unlock();
+        }
         return "OK";
     }
 
     private String handleGet(Massege mappedMassege) {
-        String value = hashMap.get(mappedMassege.getKey());
-        if (value == null) {
-            return "NOT FOUND";
+        // to get which hashmap - hashcode the key -> module with number of shards -> hashmap array index
+        int index = Math.abs(mappedMassege.getKey().hashCode()) % NUM_SHARDS;
+        String value;
+        try {
+            locks[index].readLock().lock();
+            value = shards[index].get(mappedMassege.getKey());
+            if (value == null) {
+                return "NOT FOUND";
+            }
+        } finally {
+            locks[index].readLock().unlock();
         }
         return value;
     }
 
     private String handleDel(Massege mappedMassege) {
-        String removed = hashMap.remove(mappedMassege.getKey());
-        if(removed != null)
-            return "1";
+        // to get which hashmap - hashcode the key -> module with number of shards -> hashmap array index
+        int index = Math.abs(mappedMassege.getKey().hashCode()) % NUM_SHARDS;
+
+        try {
+        locks[index].writeLock().lock();
+        String removed = shards[index].remove(mappedMassege.getKey());
+        if (removed != null) {
+            return "Deleted!";
+        }
+
+        } finally {
+            locks[index].writeLock().unlock();
+        }
         
-        return "0";
+        return "Failed To Delete";
     }
 }
